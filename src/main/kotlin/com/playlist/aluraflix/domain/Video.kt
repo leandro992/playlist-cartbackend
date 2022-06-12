5c08bba8f83c41476146c345ee04a4707b927fba
leandro992 <victorleandro100@outlook.com>
2022-06-12 18:03:46 -0300

    entity video strategy DB

--- src/main/kotlin/com/playlist/aluraflix/domain/Video.kt
+++ src/main/kotlin/com/playlist/aluraflix/domain/Video.kt
@@ -4,12 +4,13 @@ import java.time.LocalDateTime
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
-@Entity
+@Entity(name = "video")
 data class Video(
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     @Column
     val title: String,
